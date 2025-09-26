7919b09ba220163eede7d71cf416a4afe6eb4e2d
Polianskyi Dmytro <dimdemon1@gmail.com>
2025-09-26 17:31:03 +0300

    fix error pop up

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -3,9 +3,7 @@ const BASE_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods';
 export function httpClient<T>(url: string): Promise<T> {
   return fetch(`${BASE_URL}/${url}`).then(response => {
     if (!response.ok) {
-      return Promise.reject(
-        new Error(`Could not fetch ${url}, status: ${response.status}`),
-      );
+      return new Error(`Could not fetch ${url}, status: ${response.status}`);
     }
 
     return response.json();
